61a227eb52198bc904f8fcd55a04c4593187188c
ARSamogin <121831671+ARSamogin@users.noreply.github.com>
2026-05-07 09:10:40 -0300

    Password check
    Yet to be tested!

--- passcheck.js
+++ passcheck.js
@@ -1,2 +1,17 @@
 const senha1 = document.getElementById("Senha") 
 const senha2 = document.getElementById("ConfirmarSenha")
+const form = document.getElementById("FormSubmit")
+
+function comparisson(){
+  if(senha1 === senha2){
+    pass
+  }
+  else{
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      alert("Senhas Diferentes")
+    }  
+    )
+  }
+}
